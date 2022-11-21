f6fbaa1296c84d60ff0e6ae48712faf0140353ec
Kaziurr <96995175+Kaziurr@users.noreply.github.com>
2022-11-21 08:44:24 +0100

    Add files via upload

---
+++ Firma_schemat/Firma_schemat/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Firma_schemat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void dodajGalazToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 dialog = new Form2();
+            dialog.Text = "Dodawanie gałęzi";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                treeView1.Nodes.Add(dialog.nazwa);
+            }
+        }
+
+        private void dodajPodgalazToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 dialog = new Form2();
+            dialog.Text = "Dodawanie podgałęzi";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                treeView1.SelectedNode.Nodes.Add(dialog.nazwa);
+
+            }
+        }
+
+        private void zmieńToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 dialog = new Form2();
+            dialog.Text = "Modyfikowanie gałęzi";
+            dialog.nazwa = treeView1.SelectedNode.Text;
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                treeView1.SelectedNode.Text = dialog.nazwa;
+            }
+        }
+
+        private void usuńToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Remove(treeView1.SelectedNode);
+        }
+
+        public class myNode
+        {
+            public string rodzic;
+            public string nazwa;
+
+            public myNode(string rodzic, string nazwa)
+            {
+                this.rodzic = rodzic;
+                this.nazwa = nazwa;
+            }
+        }
+    }
+}
